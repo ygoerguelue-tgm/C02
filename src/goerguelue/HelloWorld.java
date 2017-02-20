db8c0932a711c67449dea466b18572ecce9430d4
ygoerguelue <ygoerguelue@student.tgm.ac.at>
2017-02-20 10:47:11 +0100

    HelloWorld

---
+++ src/goerguelue/HelloWorld.java
@@ -0,0 +1,11 @@
+package goerguelue;
+
+public class HelloWorld {
+	public static void Hello(){
+		System.out.println("Hello World");
+	}
+	
+	public static void Hallo(){
+		System.out.println("Hallo Welt");
+	}
+}
---
+++ src/goerguelue/Main.java
@@ -0,0 +1,10 @@
+package goerguelue;
+
+public class Main {
+
+	static void main(String[] args){
+		HelloWorld.Hallo();
+		HelloWorld.Hello();
+	}
+
+}
